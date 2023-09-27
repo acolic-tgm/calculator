7952e4f0d3e8f7fb6ff23ef4c8d946c7d97895d0
acolic-tgm <acolic@student.tgm.ac.at>
2023-09-27 14:17:29 +0200

    Initial commit

---
+++ app/src/main/java/at/acolic/gk7acalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package at.acolic.gk7acalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
